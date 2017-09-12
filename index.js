79b2e13ecaa8c07980d6ed95f44fb8580c2f9196
Steve Olender <nantucket.friar@gmail.com>
2017-09-12 17:48:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var obj = object;
   return Object.assign({}, obj, {[key]: value});
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  var obj = object;
+  obj[key] = value;
+  return obj;
+}
